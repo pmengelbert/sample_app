796aeab0dfe10cd128a31414ed9b3199cf8d9659
Peter Engelbert <pmengelbert@gmail.com>
2019-07-09 01:47:36 +0000

    Added a user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  get '/list', to: 'users#list'
+  post '/signup', to: 'users#create'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  resources :users
 end
