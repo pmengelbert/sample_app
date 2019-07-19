22bef2ecea1eca560204cfbe67ecd3f572ca977a
Peter Engelbert <pmengelbert@gmail.com>
2019-07-19 04:16:27 +0000

    Fixed heroku glitch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
-  host = hudson-marks-50698.herokuapp.com
+  host = 'hudson-marks-50698.herokuapp.com'
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
